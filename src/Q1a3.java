ff032e5350a28d59de78837a774b337a5ab07970
alysson <alyssos@hotmail.com>
2023-10-10 15:39:17 -0300

    fix: adicioando linha coluna para usa os 6 no

--- src/Q1a3.java
+++ src/Q1a3.java
@@ -1,14 +1,15 @@
 public class Q1a3 {
     public static void main(String[] args) {
         int[][] graf = {
-                {0,1,1,1,1},
-                {1,0,1,1,1},
-                {1,1,0,1,1},
-                {1,1,1,0,1},
-                {1,1,1,1,0}
+                {0,1,1,1,1,1},
+                {1,0,1,1,1,1},
+                {1,1,0,1,1,1},
+                {1,1,1,0,1,1},
+                {1,1,1,1,0,1},
+                {1,1,1,1,1,0}
         };
-        for(int i=0; i<5; i++) {
-            for(int j=0; j<5; j++) {
+        for(int i=0; i<6; i++) {
+            for(int j=0; j<6; j++) {
                 if(graf[i][j] > 0) {
                     System.out.print(i + "" + j + " = 1\t");
                 } else {
