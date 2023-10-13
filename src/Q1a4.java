fec6b97f6bdf18211e41e8f928fbac3359fdbef6
Alysson <alyssos@hotmail.com>
2023-10-13 16:54:31 -0300

    feat: modificaçoes para ler a matriz e impresao dos 6 no

--- src/Q1a4.java
+++ src/Q1a4.java
@@ -23,7 +23,7 @@ public class Q1a4 {
         sc.close();
 
         for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < 6; j++) {
                 if (adj[i][j] > 0) {
                     System.out.print(i + "" + j + " = 1\t");
                 } else {
@@ -34,9 +34,9 @@ public class Q1a4 {
         }
         System.out.println();
 
-        int a = 0, b = 0, c = 0, d = 0, e = 0, ae = 0, be = 0, ce = 0, de = 0, ee = 0;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        int a = 0, b = 0, c = 0, d = 0, e = 0 , f = 0, ae = 0, be = 0, ce = 0, de = 0, ee = 0, fe = 0;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
                 if ((adj[i][j] > 0) && (i == 0)) {
                     a++;
                 } else if ((adj[i][j] < 0) && (i == 0)) {
@@ -62,9 +62,14 @@ public class Q1a4 {
                 } else if ((adj[i][j] < 0) && (i == 4)) {
                     ee++;
                 }
+                if ((adj[i][j] > 0) && (i == 5)) {
+                    f++;
+                } else if ((adj[i][j] < 0) && (i == 5)) {
+                    fe++;
+                }
             }
         }
-        System.out.println("A_S=" + a + "\t B_S=" + b + "\t C_S=" + c + "\t D_S=" + d + "\t E_S=" + e + "\n");
-        System.out.println("A_E=" + ae + "\t B_E=" + be + "\t C_E=" + ce + "\t D_E=" + de + "\t E_E=" + ee + "\n");
+        System.out.println("A_S=" + a + "\t B_S=" + b + "\t C_S=" + c + "\t D_S=" + d + "\t E_S=" + e + "\t f_S=" + f +"\n");
+        System.out.println("A_E=" + ae + "\t B_E=" + be + "\t C_E=" + ce + "\t D_E=" + de + "\t E_E=" + ee + "\t F_E=" + fe + "\n");
     }
 }
