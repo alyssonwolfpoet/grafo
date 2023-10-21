c588aba72ae653e1fc5bc1db74b1fe8966cf7889
Alysson <alyssos@hotmail.com>
2023-10-21 19:59:29 -0300

    fix: grafo fix

--- src/Q1a6.java
+++ src/Q1a6.java
@@ -25,12 +25,12 @@ class Grafo3 {
     // Método que adiciona uma aresta não direcionada entre os vértices u e v
     public void addAresta(int u, int v, int e, int peso) {
         // Verifica se os índices são válidos
-        if (u >= 0 && u < n && v >= 0 && v < n && e >= 0 && e < m) {
+       // if (u >= 0 && u < n && v >= 0 && v < n && e >= 0 && e < m) {
             // Marca os vértices u e v como incidentes à aresta e
             matriz[u][e] = 1;
             matriz[v][e] = 1;
-            this.vertices[u] = peso;
-        }
+            this.vertices[e] = peso;
+       //}
     }
 
     // Método que imprime a matriz de incidência
@@ -49,19 +49,22 @@ class Q1a6 {
     public static void main(String[] args) {
         Grafo3 g = new Grafo3(7,12);
         g.addAresta(0, 0, 0,5); // Primeira aresta entre o primeiro e o segundo nó
-        g.addAresta(1, 0, 1,2); // Segunda aresta entre o segundo e o terceiro nó
-        g.addAresta(2, 1, 2,4); // Terceira aresta entre o terceiro e o quinto nó
-        g.addAresta(3, 1, 3,6); // Quarta aresta entre o quarto e o segundo nó
-        g.addAresta(4, 2, 3,5); // Quarta aresta entre o quarto e o segundo nó
-        g.addAresta(5, 1, 4,2); // Quarta aresta entre o quarto e o segundo nó
-        g.addAresta(6, 2, 5,6); // Quarta aresta entre o quarto e o segundo nó
-        g.addAresta(7, 3, 4,6); // Quarta aresta entre o quarto e o segundo nó
-        g.addAresta(8, 3, 5,6); // Quarta aresta entre o quarto e o segundo nó
-        g.addAresta(9, 4, 5,3); // Quarta aresta entre o quarto e o segundo nó
-        g.addAresta(10, 4, 6,5); // Quarta aresta entre o quarto e o segundo nó
-        g.addAresta(11, 5, 6,4); // Quarta aresta entre o quarto e o segundo nó
+        g.addAresta(0, 1, 1,3); // Segunda aresta entre o segundo e o terceiro nó
+        g.addAresta(1, 2, 2,4); // Terceira aresta entre o terceiro e o quinto nó
+        g.addAresta(1, 3, 3,6); // Quarta aresta entre o quarto e o segundo nó
+        g.addAresta(2, 3, 4,5); // Quarta aresta entre o quarto e o segundo nó
+        g.addAresta(1, 4, 5,2); // Quarta aresta entre o quarto e o segundo nó
+        g.addAresta(2, 5, 6,6); // Quarta aresta entre o quarto e o segundo nó
+        g.addAresta(3, 4, 7,6); // Quarta aresta entre o quarto e o segundo nó
+        g.addAresta(3, 5, 8,6); // Quarta aresta entre o quarto e o segundo nó
+        g.addAresta(4, 5, 9,3); // Quarta aresta entre o quarto e o segundo nó
+        g.addAresta(4, 6, 10,5); // Quarta aresta entre o quarto e o segundo nó
+        g.addAresta(5, 6, 11,4); // Quarta aresta entre o quarto e o segundo nó
         g.printMatriz();
 
+        for (int i = 0; i < g.vertices.length; i++) {
+            System.out.print(g.vertices[i]);
+        }
 
 
     }
